6dbccf93b8de3b707d7876ef4806bc85ce361522
rzagza039@gmail.com <rzagza039@gmail.com>
2023-08-07 17:20:59 -0500

    springBoot en visualstudio creacion de crud

--- src/main/java/com/example/demo/controller/usuarioControler.java
+++ src/main/java/com/example/demo/controller/usuarioControler.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.models.usuario;
@@ -7,8 +8,38 @@ import com.example.demo.models.usuario;
 @RestController
 public class usuarioControler {
     
-    @RequestMapping(value = "usuario")
-    public usuario getUsuario(){
+    @RequestMapping(value = "usuario/{id}")
+    public usuario create(@PathVariable int id){
+        usuario usuario = new usuario();
+        usuario.setId(3);
+        usuario.setCorreo("samir@gmail");
+        usuario.setDireccion("calle 55");
+        usuario.setNombre("samir");
+
+        return usuario;
+    }
+     @RequestMapping(value = "update")
+     public usuario update(){
+        usuario usuario = new usuario();
+        usuario.setId(3);
+        usuario.setCorreo("samir@gmail");
+        usuario.setDireccion("calle 55");
+        usuario.setNombre("samir");
+
+        return usuario;
+    }
+     @RequestMapping(value = "read")  
+     public usuario read(){
+        usuario usuario = new usuario();
+        usuario.setId(3);
+        usuario.setCorreo("samir@gmail");
+        usuario.setDireccion("calle 55");
+        usuario.setNombre("samir");
+
+        return usuario;
+    }
+     @RequestMapping(value = "delete")
+     public usuario delete(){
         usuario usuario = new usuario();
         usuario.setId(3);
         usuario.setCorreo("samir@gmail");
